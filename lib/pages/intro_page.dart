3da04ce757bd68d037f434ad67d50348760957ed
Selim <developershady00@gmail.com>
2024-09-28 21:31:18 +0300

    Files Upload

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          // Nike logo
+
+          // Just do it text
+
+          // Describe
+
+          // Shop Now Button
+        ],
+      ),
+    );
+  }
+}
