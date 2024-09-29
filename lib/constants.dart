5b0a8c5d6ad6f91828a064b9794773da6f2c4709
Selim <developershady00@gmail.com>
2024-09-29 10:16:06 +0300

    Files Upload

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const TextStyle kIntroTitle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 26,
+);
+
+const TextStyle kIntroSubTitle = TextStyle(
+  color: Colors.grey,
+  fontSize: 18,
+);
+
+const TextStyle kIntroButton = TextStyle(
+  color: Colors.white,
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sneaker_shop/constants.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -7,16 +8,59 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          // Nike logo
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 26),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Nike logo
+            Image.asset(
+              "lib/images/nike.png",
+              width: 250,
+            ),
 
-          // Just do it text
+            const SizedBox(
+              height: 46,
+            ),
 
-          // Describe
+            // Just do it text
+            const Text(
+              "Just Do It",
+              style: kIntroTitle,
+            ),
 
-          // Shop Now Button
-        ],
+            const SizedBox(
+              height: 16,
+            ),
+
+            // Describe
+            const Text(
+              "Brand new sneakers and custom kicks made with premium quality",
+              style: kIntroSubTitle,
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(
+              height: 46,
+            ),
+
+            // Shop Now Button
+            Container(
+              width: double.infinity,
+              height: 66,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: const Center(
+                child: Text(
+                  "Shop Now",
+                  style: kIntroButton,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
